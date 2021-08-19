4b83e1ceba431fb7d4f17a4c0b8111b1e1f9091e
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-08-19 12:30:37 +0100

    Endpoint and networking components implemented.

---
+++ Users/Core/Networking/Endpoint.swift
@@ -0,0 +1,13 @@
+//
+//  Endpoint.swift
+//  Users
+//
+//  Created by Charlie Nguyen on 19/08/2021.
+//
+
+import Foundation
+
+struct Endpoint {
+    var path: String
+    var queryItems: [URLQueryItem] = []
+}

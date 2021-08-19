15a2849d4197f044df76a6c3f2625c3571727eca
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-08-19 12:10:02 +0100

    Initial Commit

---
+++ Users/UsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersApp.swift
+//  Users
+//
+//  Created by Charlie Nguyen on 19/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct UsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

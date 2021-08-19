fd21cc961ec88c2d7f2fad5e75bca648fc0eeca0
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-08-19 12:45:50 +0100

    Models for users and fake user for swift previews

---
+++ Users/Business Logic/Models/User.swift
@@ -0,0 +1,34 @@
+//
+//  User.swift
+//  Users
+//
+//  Created by Charlie Nguyen on 19/08/2021.
+//
+
+import Foundation
+
+struct User: Codable, Identifiable {
+    let id: String?
+    let title: String?
+    let firstName: String
+    let lastName: String
+    let email: String
+    let dateOfBirth: String?
+    let registerDate: String?
+    let phone: String?
+    let picture: String?
+}
+
+extension User {
+    static func fakeUser() -> Self {
+        return User(id: "123",
+                    title: "Test",
+                    firstName: "Charlie",
+                    lastName: "Nguyen",
+                    email: "test@gmail.com",
+                    dateOfBirth: "31/12/1994",
+                    registerDate: "1/1/2020",
+                    phone: "+123456",
+                    picture: nil)
+    }
+}
---
+++ Users/Business Logic/Models/Users.swift
@@ -0,0 +1,12 @@
+//
+//  Users.swift
+//  Users
+//
+//  Created by Charlie Nguyen on 19/08/2021.
+//
+
+import Foundation
+
+struct Users: Codable {
+    let data: [User]
+}
